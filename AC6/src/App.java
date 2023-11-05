f86aa3bbcc75782ff4b55a8dbe9ac35151038a0c
Matheus Liporace <108703224+LiporaceM@users.noreply.github.com>
2023-11-05 13:25:54 -0300

    Add files via upload

---
+++ AC6/src/App.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+import entidades.AlunoDAO;
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        AlunoDAO alunos = new AlunoDAO();
+        alunos.carregarDados();
+        boolean executando = true;
+        Scanner leitor = new Scanner(System.in);
+
+        while (executando) {
+            System.out.println("O que deseja fazer?\n  1 - Listar todos os alunos\n  2 - Buscar aluno por matrícula\n  3 - Adicionar aluno\n  4 - Remover aluno\n  5 - Editar curso\n  6 - Salvar e sair\n");
+
+            int opcao = leitor.nextInt();
+            leitor.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println(alunos);
+                    break;
+
+                case 2:
+                    System.out.println(alunos.buscarMatricula(leitor) + "\n");
+                    break;
+
+                case 3:
+                    alunos.adicionarAluno(leitor);
+                    break;
+
+                case 4:
+                    alunos.removerAluno(leitor);
+                    break;
+
+                case 5:
+                    alunos.atualizarCurso(leitor);
+                    break;
+
+                case 6:
+                    alunos.salvarDados();
+                    executando = false;
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        }
+
+        leitor.close();
+
+    }
+}
---
+++ AC6/src/entidades/AlunoDAO.java
@@ -0,0 +1,125 @@
+package entidades;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+    public class AlunoDAO {
+        private List<Aluno> alunos;
+        private final String arquivoAlunos = "alunos.txt";
+
+        public AlunoDAO() {
+            this.alunos = new ArrayList<>();
+        }
+
+        public void carregarDados() {
+            try (BufferedReader reader = new BufferedReader(new FileReader(arquivoAlunos))) {
+                String linha;
+                while ((linha = reader.readLine()) != null) {
+                    String[] dados = linha.split("\\|");
+                    if (dados.length == 3) {
+                        String nome = dados[0];
+                        String curso = dados[1];
+                        String matricula = dados[2];
+                        alunos.add(new Aluno(nome, curso, matricula));
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println("Erro ao carregar dados: " + e.getMessage());
+            }
+        }
+
+        public void salvarDados() {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(arquivoAlunos, false))) {
+                for (Aluno aluno : alunos) {
+                    writer.write(aluno.getNome() + "|" + aluno.getCurso() + "|" + aluno.getMatricula());
+                    writer.newLine();
+                }
+            } catch (IOException e) {
+                System.err.println("Erro ao salvar dados: " + e.getMessage());
+            }
+        }
+
+        public void adicionarAluno(Scanner leitor) {
+            System.out.print("Digite a matrícula do aluno: ");
+            String matricula = leitor.nextLine();
+            if (matricula.equals("")){
+                System.out.print("Opção Invalida! Digite a matrícula do aluno novamente: ");
+                matricula = leitor.nextLine();
+            }
+            System.out.print("Digite o nome do aluno: ");
+            String nome = leitor.nextLine();
+            if (nome.equals("")){
+                System.out.print("Opção Invalida! Digite o nome do aluno novamente: ");
+                nome = leitor.nextLine();
+            }
+            System.out.print("Digite o curso do aluno: ");
+            String curso = leitor.nextLine();
+            if (curso.equals("")){
+                System.out.print("Opção Invalida! Digite o curso do aluno novamente: ");
+                curso = leitor.nextLine();
+            }
+            this.alunos.add(new Aluno(nome, curso, matricula));
+            System.out.println("Aluno adicionado com sucesso!");
+        }
+
+        public void removerAluno(Scanner leitor) {
+            System.out.print("Informe a matrícula do aluno a ser removido: ");
+            String matricula = leitor.nextLine();
+            for (Aluno aluno : alunos) {
+                if (aluno.getMatricula().equals(matricula)) {
+                    alunos.remove(aluno);
+                    System.out.println("Aluno removido com sucesso!");
+                    break;
+                }
+            }
+            System.out.println("Matricula não encontrada!");
+        }
+
+        public void atualizarCurso(Scanner leitor) {
+            System.out.print("Digite a matrícula do aluno: ");
+            String matricula = leitor.nextLine();
+            for (Aluno aluno : alunos) {
+                if (aluno.getMatricula().equals(matricula)) {
+                    System.out.println("Aluno encontrado: " + aluno.getNome());
+                    System.out.println("Curso atual: " + aluno.getCurso());
+                    System.out.print("Digite o novo curso do aluno: ");
+                    String novoCurso = leitor.nextLine();
+                    if (novoCurso.equals("")){
+                    System.out.print("Opção Invalida! Digite o novo curso do aluno novamente: ");
+                    novoCurso = leitor.nextLine();
+            }
+                    aluno.setCurso(novoCurso);
+                    return;
+                }
+            }
+            System.err.println("Matrícula não encontrada!\n");
+        }
+
+        public Aluno buscarMatricula(Scanner leitor) {
+            System.out.print("Digite a matrícula do aluno: ");
+            String matricula = leitor.nextLine();
+            for (Aluno aluno : alunos) {
+                if (aluno.getMatricula().equals(matricula)) {
+                    return aluno;
+                }
+            }
+            System.out.println("Matricula não encontrada!");
+            return null;
+        }
+
+        public String toString() {
+            String listaAlunos = "";
+
+            for (Aluno aluno : alunos) {
+                listaAlunos += aluno + "\n\n";
+            }
+            return listaAlunos;
+        }
+    }
+
